8e6e8929f00d76511d68e05868f7c6d8ef5d3e4e
Baker Smurf <38057213+bakersmurf@users.noreply.github.com>
2020-03-09 20:23:38 -0700

    playing with window resizing

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
-let img = new Image();
-img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
-img.onload = function() {
-  init();
-};
-
-let canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
-
-function init() {
-  ctx.drawImage(img, 0, 0, 16, 18, 0, 0, 16, 18);
+window.onload = window.onresize = function() {
+    var canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth * 0.8;
+    canvas.height = window.innerHeight * 0.8;
 }
