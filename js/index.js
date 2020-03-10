13b95669e94d76b931decd9d8bd9761931f3cecd
Baker Smurf <38057213+bakersmurf@users.noreply.github.com>
2020-03-09 20:50:44 -0700

    window centers!

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload = window.onresize = function() {
-    var canvas = document.getElementById('canvas');
+    var canvas = document.getElementById('GameCanvas');
     canvas.width = window.innerWidth * 0.8;
     canvas.height = window.innerHeight * 0.8;
 }
